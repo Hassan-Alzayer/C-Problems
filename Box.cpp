bf9ba9348faa16bd7cae9675778844f1b996010f
HassanCode985 <108063654+HassanCode985@users.noreply.github.com>
2022-07-29 19:26:10 +0300

    Create Box.cpp

---
+++ Box.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    //a balls remove
+    //n balls contained
+    //b new balls
+    int a , n , b ;
+    cin>>n;
+    cin>>a;
+    cin>>b;
+    cout<<n+b-a;
+    return 0;
+}
+
+
