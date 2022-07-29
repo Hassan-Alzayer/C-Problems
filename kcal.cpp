fc95dbc081ae8880b6bacd05054e00fb0e0fed23
HassanCode985 <108063654+HassanCode985@users.noreply.github.com>
2022-07-29 22:32:24 +0300

    Create kcal.cpp

---
+++ kcal.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    //Drink A have 100 milic
+    //Drink B have x millic
+    double a ,b ;
+    cin>>a;
+    cin>>b;
+    cout<<(a*b)/100;
+
+
+
+
+
+    return 0;
+}
