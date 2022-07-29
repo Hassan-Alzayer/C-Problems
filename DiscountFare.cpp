efcd85596032f11d41c41b702304a72985cb3058
HassanCode985 <108063654+HassanCode985@users.noreply.github.com>
2022-07-29 21:55:22 +0300

    Create DiscountFare.cpp

---
+++ DiscountFare.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    //From A to B by train = 1
+    //From b to c by bus = 1/2
+    int x;
+    int y;
+    cin>>x;
+    cin>>y;
+
+    cout<<(y/2)+x;
+
+    return 0;
+}
+
+
