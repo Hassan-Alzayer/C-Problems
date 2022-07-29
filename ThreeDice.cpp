7701d36690ad8e09eeffb8181d0b745474ae5269
HassanCode985 <108063654+HassanCode985@users.noreply.github.com>
2022-07-29 19:44:00 +0300

    Create ThreeDice.cpp

---
+++ ThreeDice.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    //a dice one
+    //b dice one
+    //c dice one
+    int a , c , b ;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cout<<(7-a)+(7-b)+(7-c);
+    return 0;
+}
+
+
