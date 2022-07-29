e7348853072dc5c8fdc934c6767c0276d1e0e91e
HassanCode985 <108063654+HassanCode985@users.noreply.github.com>
2022-07-29 19:15:16 +0300

    Add files via upload

---
+++ Brick.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+  //  N for kilo
+  // W for Weight
+   int n;
+   int w;
+    cout<<"enter the N kilo";
+    cin>>n;
+    cout<<"enter the W Weight";
+    cin>>w;
+    cout<<n/w;
+
+
+    return 0;
+}
