a4de1a7346d3ca9d84f47546483f94f7c2963190
HassanCode985 <108063654+HassanCode985@users.noreply.github.com>
2022-07-30 18:12:12 +0300

    Create DominoPiling.cpp

---
+++ DominoPiling.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    //m*n
+    //2*1 unlimited number of standard domino pieces
+
+    int a ,b,c;
+    cin>>a;
+    cin>>b;
+    cout<<a*b/2;
+    return 0;
+}
+
+
